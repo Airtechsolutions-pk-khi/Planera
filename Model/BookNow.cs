f32ee3efb1e7e929c864596aca12ddb6b08078d9
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2022-08-16 20:13:29 +0500

    16-08-2022 Book NOw

---
+++ Model/BookNow.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Planera.Model
+{
+    public class BookNow
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Service { get; set; }
+        public string Time { get; set; }
+        public string Details { get; set; }
+    }
+}
